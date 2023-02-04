6b6ac9a374ea41338c98e8a4930bcf0b7b22c51d
Technician.Abdilahi MA <124462693+Hayow123@users.noreply.github.com>
2023-02-04 11:39:06 -0800

    done by three files

---
+++ javascript.js
@@ -0,0 +1,5 @@
+let carName1 = "Volvo XC60";  // Double quotes
+let carName2 = 'Volvo XC60';  // Single quotes
+let answer1 = "It's alright";
+let answer2 = "He is called 'Johnny'";
+let answer3 = 'He is called "Johnny"';
